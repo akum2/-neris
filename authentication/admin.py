91831a1fff9a0a1f594fd1efa5f0d362030ab5dd
yokwejuste <yokwejuste@yahoo.com>
2022-04-25 14:49:42 +0100

    updates

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+from authentication.models import Try
+
+admin.site.register(Try)
+# admin.site.unregister(User)
+# admin.site.unregister(Group)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Try(models.Model):
+    name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'Try'
+        verbose_name_plural = 'Tries'
