76413fb2ecc1d418f5623125f3c08065e8cb40dd
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-02 05:28:14 +0100

    feat: code cleaning

--- authentication/views.py
+++ authentication/views.py
@@ -65,67 +65,52 @@ def contact(request):
 def upload(request):
     if request.POST:
         universal_set_of_unique_words = []
-
-        input_query = request.POST.get("query")
-        print(input_query)
-
-        lowercase_query = open(input_query, "r").read().lower()
-        print(lowercase_query)
-
+        input_query = request.POST.get("hidden")
+        lowercase_query = input_query.lower()
         query_word_list = re.sub("[\w]", " ", lowercase_query).split()
-
         for word in query_word_list:
             if word not in universal_set_of_unique_words:
                 universal_set_of_unique_words.append(word)
-
         fd = open(os.path.join(BASE_DIR, "database1.txt"), "r")
         database1 = fd.read().lower()
-
         database_word_list = re.sub("[\w]", " ", database1).split()
-
         for word in database_word_list:
             if word not in universal_set_of_unique_words:
                 universal_set_of_unique_words.append(word)
-
         query_tf = []
         database_tf = []
-
         for word in universal_set_of_unique_words:
             query_tf_counter = 0
             database_tf_counter = 0
-
             for word2 in query_word_list:
                 if word == word2:
                     query_tf_counter += 1
             query_tf.append(query_tf_counter)
-
             for word2 in database_word_list:
                 if word == word2:
                     database_tf_counter += 1
             database_tf.append(database_tf_counter)
-
         dot_product = 0
         for i in range(len(query_tf)):
             dot_product += query_tf[i] * database_tf[i]
-
         query_vector_magnitude = 0
         for i in range(len(query_tf)):
             query_vector_magnitude += query_tf[i] ** 2
         query_vector_magnitude = math.sqrt(query_vector_magnitude)
-
         database_vector_magnitude = 0
         for i in range(len(database_tf)):
             database_vector_magnitude += database_tf[i] ** 2
         database_vector_magnitude = math.sqrt(database_vector_magnitude)
-
         match_percentage = float(dot_product / (query_vector_magnitude * database_vector_magnitude)) * 100
         output = "Input query text matches %0.02f%% with database." % match_percentage
         context = {
             'output': output,
+            'val': math.ceil(match_percentage),
             'query': input_query,
         }
         return render(request, 'homepages/upload.html', context)
     else:
         context = {
+            'val': 'nothing'
         }
     return render(request, 'homepages/upload.html', context)
