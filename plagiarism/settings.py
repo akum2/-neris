8c4d415f6fee291b6812b31bb641eb0d8cd948e5
Steve Yonkeu <yokwejuste@yahoo.com>
2022-06-05 17:18:34 +0100

    feat: added login, logout and activating messages

--- plagiarism/settings.py
+++ plagiarism/settings.py
@@ -137,3 +137,8 @@ MESSAGE_TAGS = {
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
  }
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGOUT_REDIRECT_URL = 'signin'
