33083073ac2bb8acdfd3b9178de53d4d67675d76
Steve Yonkeu <yokwejuste@yahoo.com>
2022-07-02 06:24:59 +0100

    feat: made changes to the form

--- authentication/forms.py
+++ authentication/forms.py
@@ -7,11 +7,11 @@ from authentication.models import TheUsers, UploadedDocuments
 
 
 class UserLoginForm(ModelForm):
-    password = forms.CharField(label='password', widget=forms.PasswordInput(
-        attrs={"class": 'form-control form-control-user'}
+    password = forms.CharField(label = 'password', widget = forms.PasswordInput(
+        attrs = {"class": 'form-control form-control-user'}
     ))
-    username = forms.CharField(label='username', widget=forms.TextInput(
-        attrs={"class": 'form-control form-control-user', }
+    username = forms.CharField(label = 'username', widget = forms.TextInput(
+        attrs = {"class": 'form-control form-control-user', }
     ))
 
     class Meta:
@@ -23,21 +23,21 @@ class UserLoginForm(ModelForm):
             username = self.cleaned_data['username']
             password = self.cleaned_data["password"]
 
-            if not authenticate(username=username, password=password):
+            if not authenticate(username = username, password = password):
                 raise forms.ValidationError('Invalid Entries')
 
 
 class UserRegistrationForm(UserCreationForm):
-    password1 = forms.CharField(label="Password", max_length=20,
-                                widget=forms.PasswordInput(
-                                    attrs={"class": 'form-control form-control-user'
-                                           }
+    password1 = forms.CharField(label = "Password", max_length = 20,
+                                widget = forms.PasswordInput(
+                                    attrs = {"class": 'form-control form-control-user'
+                                             }
                                 )
                                 )
-    password2 = forms.CharField(label="Confirm Password", max_length=20,
-                                widget=forms.PasswordInput(
-                                    attrs={"class": 'form-control form-control-user'
-                                           }
+    password2 = forms.CharField(label = "Confirm Password", max_length = 20,
+                                widget = forms.PasswordInput(
+                                    attrs = {"class": 'form-control form-control-user'
+                                             }
                                 )
                                 )
 
@@ -46,22 +46,22 @@ class UserRegistrationForm(UserCreationForm):
         fields = ('username', 'name', 'email',
                   'phone', 'password1', 'password2')
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control form-control-user'}),
-            'name': forms.TextInput(attrs={'class': 'form-control form-control-user'}),
-            'email': forms.TextInput(attrs={'class': 'form-control form-control-user'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control form-control-user'}),
+            'username': forms.TextInput(attrs = {'class': 'form-control form-control-user'}),
+            'name': forms.TextInput(attrs = {'class': 'form-control form-control-user'}),
+            'email': forms.TextInput(attrs = {'class': 'form-control form-control-user'}),
+            'phone': forms.TextInput(attrs = {'class': 'form-control form-control-user'}),
         }
 
 
 class UpdateUserForm(forms.ModelForm):
-    name = forms.CharField(max_length=100,
-                           required=True,
-                           widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'class': 'form-control'}))
-    profile = forms.ImageField(required=False, )
+    name = forms.CharField(max_length = 100,
+                           required = True,
+                           widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    email = forms.EmailField(required = True,
+                             widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    phone = forms.EmailField(required = True,
+                             widget = forms.TextInput(attrs = {'class': 'form-control'}))
+    profile = forms.ImageField(required = False, )
 
     class Meta:
         model = TheUsers
@@ -71,14 +71,31 @@ class UpdateUserForm(forms.ModelForm):
 class UploadedDocumentsForm(forms.ModelForm):
     class Meta:
         model = UploadedDocuments
-        fields = "__all__"
+        fields = ['document', 'user', 'plagiarism_status', 'serialised_content']
         widgets = {
             'document': forms.FileInput(
-                attrs={
+                attrs = {
                     'class': 'drop-zone__input',
                     'id': 'file',
                     'accept': '.doc, .docx, .txt',
                     'required': True,
                 }
             ),
+            'user': forms.Select(
+                attrs = {
+
+                }
+            ),
+            'plagiarism_status': forms.TextInput(
+                attrs = {
+                    'hidden': True,
+                    'value': 'new thing',
+                }
+            ),
+            'serialised_content': forms.TextInput(
+                attrs = {
+                    'hidden': True,
+                    'value': 'something',
+                }
+            )
         }
