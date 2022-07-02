fddc49340c33d89fe1561a647c1558b031f717be
Steve Yonkeu <yokwejuste@yahoo.com>
2022-07-02 06:25:32 +0100

    feat: added the route or path to the record table

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('home', views.welcome, name="welcome"),
-    path('signup', views.signup, name="signup"),
-    path('signin', views.signin, name="signin"),
-    path('profile', views.profile, name="profile"),
-    path('profile-edit', views.profile_edit, name="profile-edit"),
-    path('feature', views.feature, name="feature"),
-    path('about-us', views.about, name="about-us"),
-    path('contact-us', views.contact, name="contact-us"),
-    path('upload', views.upload, name="upload"),
-    path('logout', views.logout, name="logout"),
-    path('results', views.results, name="results"),
+    path('', views.home, name = "home"),
+    path('home', views.welcome, name = "welcome"),
+    path('signup', views.signup, name = "signup"),
+    path('signin', views.signin, name = "signin"),
+    path('profile', views.profile, name = "profile"),
+    path('profile-edit', views.profile_edit, name = "profile-edit"),
+    path('feature', views.feature, name = "feature"),
+    path('about-us', views.about, name = "about-us"),
+    path('contact-us', views.contact, name = "contact-us"),
+    path('upload', views.upload, name = "upload"),
+    path('logout', views.logout, name = "logout"),
+    path('results', views.results, name = "results"),
+    path('uploaded', views.downloadable, name = 'uploaded')
 ]
